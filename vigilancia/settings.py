3997150d57831d8be7572d4bcf1de385dc0b142d
Diego-unemi <dneirag@unemi.edu.ec>
2024-12-03 23:36:09 -0500

    cambios funcionales

--- vigilancia/settings.py
+++ vigilancia/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'vigilancia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'databasews',  # Nombre que especificaste al crear la BD
-        'USER': 'postgres',                       # Usuario de la base de datos
-        'PASSWORD': 'mysuperuser',           # Contraseña que configuraste
-        'HOST': 'databasews.chwqwugmkavl.us-east-2.rds.amazonaws.com',  # Endpoint de RDS
-        'PORT': '5432',                        # Puerto para PostgreSQL
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'databasews',  # Nombre que especificaste al crear la BD
+#         'USER': 'postgres',                       # Usuario de la base de datos
+#         'PASSWORD': 'mysuperuser',           # Contraseña que configuraste
+#         'HOST': 'databasews.chwqwugmkavl.us-east-2.rds.amazonaws.com',  # Endpoint de RDS
+#         'PORT': '5432',                        # Puerto para PostgreSQL
+#     }
+# }
 
 
 # Password validation
